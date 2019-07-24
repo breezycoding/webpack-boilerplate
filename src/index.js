4fae995ed30a5806719c8cbe39fd738e42caa4cc
ip <breezycoder@gmail.com>
2019-07-24 16:09:29 +0800

    adding fontawesome

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+import {library, dom} from "@fortawesome/fontawesome-svg-core";
+import { faSpinner, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faAccessibleIcon } from "@fortawesome/free-brands-svg-icons";
 import "./index.css";
 import "./index.scss";
+
+library.add(faSpinner, faUser, faAccessibleIcon);
+dom.watch();
+
