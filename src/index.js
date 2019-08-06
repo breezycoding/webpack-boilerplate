f09a55afa3c6c262389b31215915b28662b4b6aa
ip <breezycoder@gmail.com>
2019-08-06 13:35:27 +0800

    arrange scss folder structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {library, dom} from "@fortawesome/fontawesome-svg-core";
 import { faSpinner, faUser } from "@fortawesome/free-solid-svg-icons";
 import { faAccessibleIcon } from "@fortawesome/free-brands-svg-icons";
 import "./index.css";
-import "./index.scss";
+import "./assets/styles/styles.scss";
 import { catImage } from "./components/addImage";
 
 library.add(faSpinner, faUser, faAccessibleIcon);
